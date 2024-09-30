7f6d8b62c3e7386d85db75657a7b509d87c5bb3e
Sulkifli Asmunandar <kflmattuk@gmail.com>
2024-09-30 19:24:48 +0800

    feat(profile): create profile schema
    create profile schema and relations to users table

--- src/users/schema.ts
+++ src/users/schema.ts
@@ -1,4 +1,5 @@
-import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
+import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
   id: serial('id').primaryKey(),
@@ -10,3 +11,20 @@ export const users = pgTable('users', {
     .defaultNow()
     .$onUpdateFn(() => new Date(Date.now())),
 });
+
+export const profiles = pgTable('profiles', {
+  id: serial('id').primaryKey(),
+  firstName: text('first_name').notNull(),
+  lastName: text('last_name').notNull(),
+  age: integer('age').notNull(),
+  about: text('about'),
+  userId: integer('user_id').references(() => users.id),
+});
+
+export const usersRelations = relations(users, ({one}) => ({
+  user_profile: one(profiles)
+}))
+
+export const profilesRelations = relations(profiles, ({one}) => ({
+  user: one(users, {fields: [profiles.userId], references: [users.id]})
+}))
